5a7e0bd0d11d67a9018fb7160885a32737bb9641
Esteban <esteban.barneaud@orange.fr>
2024-04-05 22:22:50 +0200

    Création des bases du dataframe

---
+++ src/main/java/projetdevops/Couple.java
@@ -0,0 +1,12 @@
+package projetdevops;
+
+public class Couple<E,V> {
+
+    public E first;
+    public V second;
+
+    Couple(E first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+}
---
+++ src/main/java/projetdevops/Dataframe.java
@@ -0,0 +1,9 @@
+package projetdevops;
+
+import java.util.ArrayList;
+
+public class Dataframe {
+    ArrayList<Couple<String,Class>> columnsNamesAndClasses;
+
+    ArrayList<ArrayList> data;
+}
